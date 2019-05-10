9245267009a9b1ec4b7b722bbb8dd143efcfbe47
Tony Kieling <tony.kieling@gmail.com>
2019-05-09 17:34:52 -0700

    dealing with class component

---
+++ src/App-bckp.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import createPersistedState from "use-persisted-state";
+
+import Login from './components/Login.js'
+import Counter from './components/Counter.js'
+
+// const Counter = ({ id }) => {
+//   console.log("id: ", id)
+//   const useCounterState = createPersistedState(id);
+//   const [count, setCount] = useCounterState(0);
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>Click me</button>
+//     </div>
+//   );
+// };
+
+// const Login = ({ login }) => {
+//   console.log("App-login:", login)
+//   return login ? <h1>Hello, {login}</h1> : <h1>Not logged in</h1>;
+// };
+
+
+function App() {
+  // Declare a new state variable, which we'll call "count"
+  const useLoginState = createPersistedState("");
+  const [login, setLogin] = useLoginState("");
+
+  return (
+    <div>
+      <Counter id="id1" />
+      <Counter id="id2" />
+      <input type="text" onChange={event => setLogin(event.target.value)} />
+      <Login login={login} />
+    </div>
+  );
+}
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import createPersistedState from "use-persisted-state";
 
-import Login from './components/Login.js'
 import Counter from './components/Counter.js'
 
-// const Counter = ({ id }) => {
-//   console.log("id: ", id)
-//   const useCounterState = createPersistedState(id);
-//   const [count, setCount] = useCounterState(0);
-//   return (
-//     <div>
-//       <p>You clicked {count} times</p>
-//       <button onClick={() => setCount(count + 1)}>Click me</button>
-//     </div>
-//   );
-// };
 
-// const Login = ({ login }) => {
-//   console.log("App-login:", login)
-//   return login ? <h1>Hello, {login}</h1> : <h1>Not logged in</h1>;
-// };
+import React, { Component } from 'react'
 
-
-function App() {
-  // Declare a new state variable, which we'll call "count"
-  const useLoginState = createPersistedState("");
-  const [login, setLogin] = useLoginState("");
-
-  return (
-    <div>
-      <Counter id="id1" />
-      <Counter id="id2" />
-      <input type="text" onChange={event => setLogin(event.target.value)} />
-      <Login login={login} />
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Counter id="id1" />
+        <Counter id="id2" />
+      </div>
+    )
+  }
 }
-export default App;
 
 ReactDOM.render(<App />, document.getElementById("root"));
