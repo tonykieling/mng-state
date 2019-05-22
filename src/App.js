ebd7307e4cf26ed8c0a2904dbec830f7a12b8dd0
Tony Kieling <tony.kieling@gmail.com>
2019-05-22 16:53:37 -0700

    Login is working, but recording inside itiself - no external Storage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import createPersistedState from "use-persisted-state";
+// import createPersistedState from "use-persisted-state";
 
-// import Login from './components/Login.js'
+import Login from './components/Login.js'
 import Counter from './components/Counter.js'
 
-const Login = ({ login }) => {
-  // console.log("App-login:", login)
-  return login ? <h1>Hello, {login}</h1> : <h1>Not logged in</h1>;
-};
+// const Login = ({ login }) => {
+//   // console.log("App-login:", login)
+//   return login ? <h1>Hello, {login}</h1> : <h1>Not logged in</h1>;
+// };
 
 // const Counter = ({ id }) => {
 //   console.log("id: ", id)
@@ -25,15 +25,16 @@ const Login = ({ login }) => {
 
 function App() {
   // Declare a new state variable, which we'll call "count"
-  const useLoginState = createPersistedState("");
-  const [login, setLogin] = useLoginState("");
+  // const useLoginState = createPersistedState("");
+  // const [login, setLogin] = useLoginState("");
   
   return (
     <div>
       <Counter id="id1" />
       <Counter id="id2" />
-      <input type="text" onChange={event => setLogin(event.target.value)} />
-      <Login login={login} />
+      {/* <input type="text" onChange={event => setLogin(event.target.value)} /> */}
+      {/* <Login login={login} /> */}
+      <Login />
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import createPersistedState from "use-persisted-state";
 
 export default function Login(props) {
+  const useLoginState = createPersistedState("");
+  const [login, setLogin] = useLoginState("");
+
   return (
     <div>
-      {props.login ?
-        (<h1>Hello, {props.login}</h1>) : 
+      <input type="text" onChange={event => setLogin(event.target.value)} />
+      {login ?
+        (<h1>Hello, {login}</h1>) : 
         (<h1>Not logged in</h1>)
       }
     </div>
