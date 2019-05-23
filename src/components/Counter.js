f457276c2c9c7c7cbe281a3ce15f9923c2f17aee
Tony Kieling <tony.kieling@gmail.com>
2019-05-22 17:00:21 -0700

    Counter improvement

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,7 +7,7 @@ export default function Counter({ id }) {
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setCount(count + 1)}>Plus</button>
       <button onClick={() => setCount(count - 1)}>Minus</button>
     </div>
   );
